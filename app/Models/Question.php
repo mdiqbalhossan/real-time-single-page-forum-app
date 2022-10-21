65ec18fc8679d0ed70982c1c3fed7fde1533f60c
Iqbal Hossen <jmiqbal7265@gmail.com>
2022-10-21 19:39:24 +0600

    Relation Between Every Model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that owns the Question
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+     /**
+      * Get all of the replies for the Question
+      *
+      * @return \Illuminate\Database\Eloquent\Relations\HasMany
+      */
+     public function replies(): HasMany
+     {
+         return $this->hasMany(Reply::class);
+     }
+
+     /**
+      * Get the category that owns the Question
+      *
+      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+      */
+     public function category(): BelongsTo
+     {
+         return $this->belongsTo(Category::class);
+     }
 }
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Reply extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the question that owns the Reply
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    /**
+     * Get the user that owns the Reply
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get all of the like for the Reply
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function like(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
 }
