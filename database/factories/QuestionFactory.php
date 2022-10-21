9d052e9afe79fec1a72d1acd41462aeebc3d3799
Iqbal Hossen <jmiqbal7265@gmail.com>
2022-10-21 19:56:45 +0600

    Factory and Seed Database

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,10 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class QuestionFactory extends Factory
 {
+    
     /**
      * Define the model's default state.
      *
@@ -13,8 +17,17 @@ class QuestionFactory extends Factory
      */
     public function definition()
     {
+        $title = $this->faker->sentence();
         return [
-            //
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'body' => $this->faker->text(),
+            'category_id' => function(){
+                return Category::all()->random();
+            },
+            'user_id' => function(){
+                return User::all()->random();
+            }
         ];
     }
 }
