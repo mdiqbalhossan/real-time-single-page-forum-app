80149c624037617ac7b28a52a3654cc2ae3cbc00
Iqbal Hossen <jmiqbal7265@gmail.com>
2022-10-22 20:58:19 +0600

    Login Model Add

--- resources/js/components/AppHome.vue
+++ resources/js/components/AppHome.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <tool-bar></tool-bar>
+        <router-view></router-view>
         <app-footer></app-footer>
     </div>
 </template>
@@ -8,8 +9,9 @@
 <script>
 import ToolBar from './ToolBar'
 import AppFooter from './AppFooter'
+import Login from './login/Login'
 export default {
-    components: { ToolBar, AppFooter },
+    components: { ToolBar, AppFooter, Login },
 }
 </script>
 
